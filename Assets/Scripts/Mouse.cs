e6e4fd3a4169b054b780377d7a2bf893d9950cf9
rulerstorm <rocklule@163.com>
2015-09-23 06:44:39 +0800

    init

---
+++ Assets/Scripts/Mouse.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Mouse : MonoBehaviour {
+
+    public Text detailText;
+
+    Vector3 cameraOffset = new Vector3(-5f, 2.5f, 0);
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+            if (Physics.Raycast(camRay, out hit, 999, 1))   //999是最大距离，1表示default层
+            {
+                if (hit.collider.gameObject.tag == "Cube")   //gameObject在collider里面
+                {
+                    detailText.text = "URI=file:" + Application.dataPath + "/testDatabase.db"; ;
+
+                    cameraOffset = new Vector3(-5f, 2.5f, -6);
+
+                    string text = SQLiteHelper.queryWithTableName("Cube");
+                    detailText.text = text;
+
+                }
+                else if (hit.collider.gameObject.tag == "Cylinder")
+                {
+                    //Debug.Log("sdsdfsf");
+
+                    cameraOffset = new Vector3(-5f, 2.5f, 6);
+
+                    string text = SQLiteHelper.queryWithTableName("Cylinder");
+                    detailText.text = text;
+
+                    
+                }
+            }else
+            {
+                cameraOffset = new Vector3(-5f, 2.5f, 0);
+                detailText.text = null;
+            }
+        }
+	
+	}
+
+
+    void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, cameraOffset, 3f * Time.deltaTime);
+    }
+
+
+
+
+}
---
+++ Assets/Scripts/SQLiteHelper.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using Mono.Data.Sqlite;
+using System.Data;
+using System;
+
+
+
+public class SQLiteHelper {
+
+    static string DatabaseName = "/testDatabase.db";
+
+    static public string queryWithTableName(string tableName)
+    {
+        string conn = "URI=file:" + Application.dataPath + "/testDatabase.db"; //Path to database.
+        IDbConnection dbconn;
+        dbconn = (IDbConnection)new SqliteConnection(conn);
+        dbconn.Open(); //Open connection to the database.
+        IDbCommand dbcmd = dbconn.CreateCommand();
+
+        string sqlQuery = "SELECT * FROM " + tableName;
+        dbcmd.CommandText = sqlQuery;
+        IDataReader reader = dbcmd.ExecuteReader();
+
+        string text = tableName + "的零件列表：\n";
+        while (reader.Read())
+        {
+            //int ID = reader.GetInt32(0);
+            text += reader.GetString(1) + '\n';
+            //Debug.Log("ID= " + ID + "  name =" + name);
+        }
+  
+        reader.Close();
+        reader = null;
+        dbcmd.Dispose();
+        dbcmd = null;
+        dbconn.Close();
+        dbconn = null;
+
+        //Debug.Log(text);
+
+        return text;
+    }
+
+
+}
